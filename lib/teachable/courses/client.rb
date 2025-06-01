e469287b165555e6819e91f8f92e26b6abdcd7b2
arthurariza <tucaariza@gmail.com>
2025-06-01 16:21:36 -0300

    refactor: remove published filter from courses endpoint to show all courses

--- lib/teachable/courses/client.rb
+++ lib/teachable/courses/client.rb
@@ -4,8 +4,8 @@ class Teachable::Courses::Client < Teachable::Base
   Result = Struct.new(:courses, :number_of_pages, keyword_init: true)
 
 
-  def get(page: 1, published: true)
-    response = self.class.get(COURSES_ENDPOINT, query: { page: page, is_published: published })
+  def get(page: 1)
+    response = self.class.get(COURSES_ENDPOINT, query: { page: page })
 
     if response.code == 200
       Result.new(courses: response["courses"], number_of_pages: response["meta"]["number_of_pages"])
